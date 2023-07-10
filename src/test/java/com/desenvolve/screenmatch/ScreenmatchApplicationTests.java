a18ef1ba9ebe373061a1b8a702228796a9b07682
LUAN BRENDON <luan_brendonmacedo@hotmail.com>
2023-07-10 18:16:49 -0300

    criando o screenmatch com spring boot

---
+++ src/test/java/com/desenvolve/screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desenvolve.screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

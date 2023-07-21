3d127bd36ca9737f0b5441e1cabad18c9cee0262
LUAN BRENDON <luan_brendonmacedo@hotmail.com>
2023-07-21 20:51:38 -0300

    banco de dados, delete e editar

--- src/main/java/com/desenvolve/screenmatch/controller/FilmeController.java
+++ src/main/java/com/desenvolve/screenmatch/controller/FilmeController.java
@@ -2,8 +2,11 @@ package com.desenvolve.screenmatch.controller;
 
 import com.desenvolve.screenmatch.model.filme.Filme;
 import com.desenvolve.screenmatch.model.filme.DadosCadastroFilme;
+import com.desenvolve.screenmatch.model.filme.FilmeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +17,9 @@ import java.util.List;
 @Controller
 @RequestMapping("/filmes")
 public class FilmeController {
-    private List<Filme> filmes = new ArrayList<>();
+
+    @Autowired
+    private FilmeRepository repository;
 
     @GetMapping("/formulario")
     public String carregaPaginaFormulario() {
@@ -23,14 +28,19 @@ public class FilmeController {
 
     @GetMapping()
     public String carregaPaginaListagem(Model model) {
-        model.addAttribute("lista", filmes);
+        model.addAttribute("lista", repository.findAll());
         return "filmes/listagem";
     }
 @PostMapping()
     public String cadastraFilme(DadosCadastroFilme dados) {
         var filme = new Filme(dados);
-        filmes.add(filme);
+        repository.save(filme);
 
+        return "redirect:/filmes";
+    }
+    @DeleteMapping
+    public String removeFilme(Long id) {
+        repository.deleteById(id);
         return "redirect:/filmes";
     }
 }
---
+++ src/main/java/com/desenvolve/screenmatch/model/filme/FilmeRepository.java
@@ -0,0 +1,6 @@
+package com.desenvolve.screenmatch.model.filme;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilmeRepository extends JpaRepository<Filme, Long> {
+}
